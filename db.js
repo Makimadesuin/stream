3a0f04b961c1306edebc6aadf605c315ac691eb2
RadityaRizky069 <azazellll069@gmail.com>
2026-05-17 19:04:58 +0700

    fix: database connection string string style

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  // 👇 SELIPKAN KODE INI DI BAWAH QUEUELIMIT (JANGAN LUPA KOMA SEBELUMNYA)
+  uri: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true,
-    minVersion: 'TLSv1.2'
-  
+    rejectUnauthorized: false
   }
 });
+
+module.exports = pool;
